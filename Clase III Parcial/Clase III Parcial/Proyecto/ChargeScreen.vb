a6488521d3556e4bdc102f693cae68d179c28b71
ErnestoFlo <65908625+ErnestoFlo@users.noreply.github.com>
2020-07-18 21:32:54 -0600

    Diseño Basicp

--- Clase III Parcial/Clase III Parcial/Proyecto/ChargeScreen.vb
+++ Clase III Parcial/Clase III Parcial/Proyecto/ChargeScreen.vb
@@ -1,15 +1,12 @@
-﻿Public Class PantallaDeCarga
+﻿Public Class ChargeScreen
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         If ProgressBar1.Value < 100 Then
             ProgressBar1.Value = ProgressBar1.Value + 10
         Else
             Timer1.Enabled = False
             Me.Hide()
-            MainPage.Show()
+            Main.Show()
         End If
     End Sub
 
-    Private Sub PantallaDeCarga_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
